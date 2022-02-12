6b643b9bfe28373ddfbf73cc9f7efded978e1795
thuongnguyen01685 <sinanhsinanh@gmail.com>
2022-02-12 20:20:25 +0700

    authencation

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const authRouter = require("./routers/authRouter");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use("/api", authRouter);
+
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello" });
+});
+
+//connect to mongodb
+const URI = process.env.MONGODB_URL;
+mongoose.connect(
+  URI,
+  {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("Connected to mongodb");
+  }
+);
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
